25cef6049e3d3d4387fbb7b90c17d8e8b071c5fe
Yusuf Farah <cyufa001@edu.xamk.fi>
2022-10-21 14:10:27 +0300

    Add project files.

---
+++ FSPTask6/Models/Passenger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FSPTask6.Models
+{
+    public class Passenger
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public Passport? Passport { get; set; }
+
+    }
+}
---
+++ FSPTask6/Models/Passport.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FSPTask6.Models
+{
+    public class Passport
+    {
+        public int Id { get; set; }
+        public int? PassportNumber { get; set; }
+        public DateTime? ValidDate { get; set; }
+        public int PassengerId { get; set; }
+        public Passenger? Passenger { get; set; }
+    }
+}
